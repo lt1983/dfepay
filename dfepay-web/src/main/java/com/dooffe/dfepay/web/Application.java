e7cbef0b46afcdf14086d90d869b34ccdb9cc310
Tars <lt1983@foxmail.com>
2017-12-08 17:18:53 +0800

    Initial commit

---
+++ dfepay-web/src/main/java/com/dooffe/dfepay/web/Application.java
@@ -0,0 +1,15 @@
+package com.dooffe.dfepay.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tars
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World ! App!");
+        SpringApplication.run(Application.class, args);
+    }
+}
